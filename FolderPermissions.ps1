87445ad4fd91fa71adacd98f2348fa7535ddc273
Ted Sdoukos <TedSdoukos@hotmail.com>
2023-09-11 09:41:04 -0500

    File Update

--- FolderPermissions.ps1
+++ FolderPermissions.ps1
@@ -8,11 +8,15 @@ $ExportPath = "$PSScriptRoot\User-$Action.csv"
  
 
 foreach ($folderPath in $folderPaths.FolderPath) {
+    Write-Verbose -Message "Currently working on $folderPath" -Verbose
     $subfolders = Get-ChildItem -Path $folderPath -Directory -Recurse 
-    foreach ($subfolder in $subfolders) {        
+    foreach ($subfolder in $subfolders) { 
+        Write-Verbose -Message "Currently working on $subfolder" -Verbose       
         foreach ($user in $users.User) {
+            Write-Verbose -Message "Currently working on $user" -Verbose
             $accessRule = Get-NTFSAccess -Path $subfolder.FullName -Account $user
             if ($accessRule) {
+                Write-Verbose -Message "Found direct membership for $user on $subfolder" -Verbose
                 switch ($Action) {
                     'List' {                     
                         [PSCustomObject]@{
